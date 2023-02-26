0dff0d5500e19b4c4ede70e1b9c1b02d062d3dfd
saratedd <sara.tedesko91@gmail.com>
2023-02-26 16:06:17 +0100

    add topics responsivness

--- base/views.py
+++ base/views.py
@@ -60,7 +60,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms': rooms, 'topics': topics, 'room_count': rooms.count(), 'room_messages': room_messages}
@@ -178,3 +178,9 @@ def updateUser(request):
 
     context = {'form': form}
     return render(request, 'base/update_user.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
