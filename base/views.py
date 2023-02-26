87a2919a2fd7bcc168f5de988591abf642f4d182
saratedd <sara.tedesko91@gmail.com>
2023-02-26 12:28:21 +0100

    add edit user

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 # Create your views here.
 
 def loginPage(request):                     # there is a built in function called login zato se ova fun zove drugacije
@@ -164,3 +164,17 @@ def deleteMessage(request, pk):
         # return redirect('room', pk=room.id)
 
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form': form}
+    return render(request, 'base/update_user.html', context)
