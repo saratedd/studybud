0aa23a953ec4bec1b7ac4650a987ab816a7f1a04
saratedd <sara.tedesko91@gmail.com>
2023-02-26 16:31:07 +0100

    add recent activity responsivness

--- base/views.py
+++ base/views.py
@@ -184,3 +184,8 @@ def topicsPage(request):
     topics = Topic.objects.filter(name__icontains=q)
     context = {'topics': topics}
     return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
